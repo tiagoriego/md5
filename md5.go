fa86acc950dc13c1ff99591f8b595379b2dde437
Tiago Riego <tiago.riego@catho.com>
2019-07-08 18:15:43 -0300

    initial commit

---
+++ md5.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	switch args := os.Args[1]; args {
+	case "-f":
+		f, err := os.Open(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer func() {
+			err := f.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanLines)
+
+		var lines []string
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
+
+		if lines != nil {
+			h := md5.New()
+			line := strings.Join(lines, "\n")
+			io.WriteString(h, line)
+			fmt.Printf("%x\n", h.Sum(nil))
+		}
+
+	case "-t":
+		h := md5.New()
+		io.WriteString(h, os.Args[2])
+		fmt.Printf("%x\n", h.Sum(nil))
+	}
+}
